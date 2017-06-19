79dd04cecfb28d939ffd28a2607c7718979de2fa
kokobobo <kobo.huang@liteon.com>
2017-06-19 18:05:02 +0800

    Second green light Two if....

--- PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -13,17 +13,48 @@ namespace PotterShoppingCart
 
     public class PotterShoppingCart
     {
+        static private int _bookOrigionPrice = 100;
         public PotterShoppingCart()
         {
         }
         public int CountShippingCart(IEnumerable<OrderData> orders)
         {
-            var totalprice = 0;
+            var totalPrice = 0;            
+            var orderGroupList = new List<List<int>>();
+
+            // 把資料分成不重複的一組
             foreach (var order in orders)
-            {
-                totalprice += order.Amount * 100;
+            {                
+                for (int i = 0; i < order.Amount; i++)
+                {
+                    if (orderGroupList.Count <= i)
+                    {
+                        var ordergroup = new List<int>();
+                        ordergroup.Add(order.Volumn);
+                        orderGroupList.Add(ordergroup);
+                    }
+                    else
+                    {
+                        orderGroupList[i].Add(order.Volumn);
+                    }
+                }
             }
-            return totalprice;
+
+            // 尋訪所有組別後依個別折扣計算總價
+            foreach (var ordergroup in orderGroupList)
+            {
+                if (ordergroup.Count() > 1)
+                {
+                    totalPrice += (int)(ordergroup.Count() * _bookOrigionPrice * 0.95);
+                }
+                else
+                {
+                    totalPrice += ordergroup.Count() * _bookOrigionPrice;
+                }
+                
+            }
+
+            return totalPrice;
         }
     }
 }
