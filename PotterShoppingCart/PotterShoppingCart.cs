44fdea2b9f9f5806e2d8121c8f08266f61e29dfc
kokobobo <kobo.huang@liteon.com>
2017-06-19 14:09:33 +0800

    First Test case(買_一本_第一集_價格為_100) & production code

---
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart
+{
+    public class Order
+    {
+        public int Volumn { get; set; }
+        public int Amount { get; set; }      
+    }
+
+    public class PotterShoppingCart
+    {
+        public PotterShoppingCart()
+        {
+        }
+        public int CountShippingCart(IEnumerable<Order> order)
+        {
+            return 1;
+        }
+    }
+}
