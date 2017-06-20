79908a3ca3af86ec04d3eb3b9f855d040b7661da
kokobobo <kobo.huang@liteon.com>
2017-06-20 11:42:33 +0800

    All test case pass Production code done 覺得if太多了應該要再改

--- PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace PotterShoppingCart
 {
@@ -20,25 +19,51 @@ namespace PotterShoppingCart
         public int CountShippingCart(IEnumerable<OrderData> orders)
         {
             var totalPrice = 0;
-            var discountAmount = 0;
-            var normalAmount = 0;
+            var orderGroupList = new List<List<int>>();
 
+            // 把資料分成不重複的一組
             foreach (var order in orders)
             {
-                if (normalAmount == 0)
+                for (int i = 0; i < order.Amount; i++)
                 {
-                    normalAmount = order.Amount;
-                }
-                else
-                {
-                    discountAmount = Math.Min(normalAmount, order.Amount);
-                    normalAmount -= discountAmount;
+                    if (orderGroupList.Count <= i)
+                    {
+                        var ordergroup = new List<int>();
+                        ordergroup.Add(order.Volumn);
+                        orderGroupList.Add(ordergroup);
+                    }
+                    else
+                    {
+                        orderGroupList[i].Add(order.Volumn);
+                    }
                 }
             }
 
-            totalPrice = (int)(discountAmount * 2 * _bookOrigionPrice * 0.95);
-            totalPrice += normalAmount * _bookOrigionPrice;
- 
+            // 尋訪所有組別後依個別折扣計算總價
+            foreach (var ordergroup in orderGroupList)
+            {
+                if (ordergroup.Count == 2) // 2本95折
+                {
+                    totalPrice += (int)(ordergroup.Count * _bookOrigionPrice * 0.95);
+                }
+                else if (ordergroup.Count == 3) // 3本9折
+                {
+                    totalPrice += (int)(ordergroup.Count * _bookOrigionPrice * 0.9);
+                }
+                else if (ordergroup.Count == 4) // 4本8折
+                {
+                    totalPrice += (int)(ordergroup.Count * _bookOrigionPrice * 0.8);
+                }
+                else if (ordergroup.Count == 5) // 5本75折
+                {
+                    totalPrice += (int)(ordergroup.Count * _bookOrigionPrice * 0.75);
+                }
+                else // 原價
+                {
+                    totalPrice += ordergroup.Count * _bookOrigionPrice;
+                }
+            }
+
             return totalPrice;
         }
     }
